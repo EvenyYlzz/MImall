d9d066d27eb9d43cb59240c601c112197d23b11f
EvenyYlzz <172463074.com>
2020-08-20 01:55:21 +0800

    学习jsonp跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,18 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import jsonp from 'jsonp'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted () {
+    const url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+    jsonp(url, () => {
+      console.log('success')
+    })
   }
 }
 </script>
