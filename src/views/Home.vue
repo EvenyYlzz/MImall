a8b7aa58a1ee43b7a986e975103e06fca513b9c2
EvenyYlzz <172463074.com>
2020-08-20 02:20:27 +0800

    学习接口代理跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,21 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import jsonp from 'jsonp'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted () {
+    const url = '/activity/servicetime'
+    jsonp(url, (err, res) => {
+      console.log('success')
+      if (err) {
+        console.log(err.stack)
+      }
+    })
   }
 }
 </script>
