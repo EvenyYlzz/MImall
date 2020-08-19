1d0a94c81756e2a0fdd8aa1c2407add8ed2cd7fc
EvenyYlzz <172463074.com>
2020-08-19 17:09:59 +0800

    学习CORS跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,18 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    let url = "https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list"
+    axios.get(url).then( () => {
+      console.log('success')
+    } )
   }
 }
 </script>
