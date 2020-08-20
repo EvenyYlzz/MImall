05c7f917af0153cbdcb0f0f08f9517363c8c0279
EvenyYlzz <172463074.com>
2020-08-20 11:23:40 +0800

    项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+import jsonp from 'jsonp'
+export dafault {
+  name:'app',
+  components:{
+    
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
 
 <style>
 #app {
