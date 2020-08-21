65908d832623c4cdb7f6b2143d7b02c54f60be93
EvenyYlzz <172463074.com>
2020-08-21 16:17:57 +0800

    MOCK设置

--- src/main.js
+++ src/main.js
@@ -3,17 +3,24 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import env from './env'
+// import env from './env'
 
+const mock = true
+//  这里只能用require加载，不能用import加载，import是预加载，预加载直接编译的时候，数据就会直接被拦截，永远拿不到，我们希望通过开关来控制数据
+if (mock) {
+  require('./mock/api')
+}
 //  根据前端的跨域方式做调整 /a /b
 //  代理就直接api现在这种(代理判断不同的请求在vue.config.js进行处理)，CORS跟jsonp要env.js的判断处理
+//  影响本地json
 axios.defaults.baseURL = '/api'
 
 //  超时设置8s
 axios.defaults.timeout = 8000
 
 //  根据环境变量获取不同的请求地址
-axios.defaults.baseURL = env.baseURL
+//  影响本地json
+// axios.defaults.baseURL = env.baseURL
 
 //  接口错误拦截（接收拦截）
 axios.interceptors.response.use(function (response) {
