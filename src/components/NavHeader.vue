0717f3c486c5544c77ec422234763867d0194f92
EvenyYlzz <172463074.com>
2020-08-22 18:20:48 +0800

    完善头部组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -18,10 +18,21 @@
         <div class="nav-header">
             <div class="container">
                 <div class="header-logo">
-
+                    <a href="/#/index"></a>
                 </div>
                 <div class="header-menu">
-
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>RedMi手机</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>电视</span>
+                        <div class="children"></div>
+                    </div>
                 </div>
                 <div class="header-search">
 
@@ -38,6 +49,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import './../assets/scss/base.scss';
 .header{
     .nav-topbar{
         height: 39px;
@@ -45,9 +57,6 @@ export default {
         background-color: #333333;
         color: #B0B0B0;
         .container{
-            width: 1226px;
-            margin-right: auto;
-            margin-left: auto;
             display: flex;
             justify-content: space-between;
             align-items: center;
@@ -68,6 +77,63 @@ export default {
                     background: url('/imgs/icon-cart-checked.png') no-repeat center;
                     background-size: contain;
                     margin-right: 4px;
+                }
+            }
+        }
+    }
+    .nav-header{
+        .container{
+            height: 112px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            .header-logo{
+                display: inline-block;
+                width: 55px;
+                height: 55px;
+                background-color: #FF6600;
+                a{
+                    display: inline-block;
+                    width: 110px;
+                    height: 55px;
+                    &:before{
+                        content: ' ';
+                        display: inline-block;
+                        width: 55px;
+                        height: 55px;
+                        background: url('/imgs/mi-logo.png') no-repeat center;
+                        background-size: 55px;
+                        transition: margin .3s;
+                    }
+                    &:after{
+                        content: ' ';
+                        display: inline-block;
+                        width: 55px;
+                        height: 55px;
+                        background: url('/imgs/mi-home.png') no-repeat center;
+                        background-size: 55px;
+                    }
+                    &:hover:before{
+                        margin-left: -55px;
+                        transition: margin .3s;
+                    }
+                }
+            }
+            .header-menu{
+                display: inline-block;
+                padding-left: 209px;
+                width: 200px;
+                .item-menu{
+                    display: inline-block;
+                    color:#333333;
+                    font-weight: bold;
+                    font-size: 16px;
+                    line-height: 112px;
+                    span{
+                        cursor: pointer;
+                    }
+                    &:hover{
+                    }
                 }
             }
         }
