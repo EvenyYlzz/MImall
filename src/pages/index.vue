efeb205ad8653950aa50b37580561d1408d4cc0a
EvenyYlzz <172463074.com>
2020-08-24 22:50:15 +0800

    完成swiper2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,50 @@
 <div class="index">
     <div class="container">
         <div class="swiper-box">
+            <div class="nav-menu">
+                <ul class="menu-wrap">
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            手机
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            电视 盒子
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            笔记本 平板
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            家电 插线板
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            出行 穿戴
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            智能 路由器
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            电源 配件
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">
+                            生活 箱包
+                        </a>
+                    </li>
+                </ul>
+            </div>
             <swiper :options="swiperOption">
                 <swiper-slide v-for="(item, index) in slideList" :key="index">
                     <a :href="'/#/product/'+item.id">
@@ -85,10 +129,48 @@ export default {
 </script>
 
 <style lang="scss">
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
 .index{
     .swiper-box{
+        .nav-menu{
+            position: absolute;
+            width: 264px;
+            height: 451px;
+            z-index: 9;
+            padding: 26px 0;
+            box-sizing: border-box;
+            background-color: #55585a7a;
+            .menu-wrap{
+                .menu-item{
+                    height: 50px;
+                    line-height: 50px;
+                    a{
+                        display: block;
+                        position: relative;
+                        font-size: $fontI;
+                        // font-weight: bold;
+                        color: $colorG;
+                        padding-left: 30px;
+                        &:after{
+                            content: ' ';
+                            position: absolute;
+                            right: 30px;
+                            top: 17.5px;
+                            @include bgImg(10px,15px,'/imgs/icon-arrow.png')
+                        }
+                    }
+                    &:hover{
+                        background-color: $colorA;
+                    }
+                }
+            }
+        }
         .swiper-container{
             height: 451px;
+            .swiper-button-prev{
+                left: 274px;
+            }
             img{
                 width: 100%;
                 height: 100%;
