7f2aa17cd16e3c28c1f68d1d60b241ee1568c454
EvenyYlzz <172463074.com>
2020-08-25 00:22:10 +0800

    完成swiper3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,6 +8,16 @@
                         <a href="javascript:;">
                             手机
                         </a>
+                        <div class="children">
+                            <ul v-for="(item, index1) in menuList" :key="index1">
+                                <li v-for="(sub, index2) in item" :key="index2">
+                                    <a :href="sub?'/#/product' + sub.id :''">
+                                        <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="#">
+                                        {{sub?sub.name:'小米9'}}
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <li class="menu-item">
                         <a href="javascript:;">
@@ -122,6 +132,30 @@ export default {
                     id: '',
                     img: '/imgs/slider/slide-5.jpg'
                 }
+            ],
+            menuList: [
+                [
+                    {
+                        id: 30,
+                        img: '/imgs/item-box-1.png',
+                        name: '小米CC9'
+                    },
+                    {
+                        id: 31,
+                        img: '/imgs/item-box-2.png',
+                        name: '小米8青春版'
+                    },
+                    {
+                        id: 32,
+                        img: '/imgs/item-box-3.jpg',
+                        name: 'Redmi K20 Pro'
+                    },
+                    {
+                        id: 33,
+                        img: '/imgs/item-box-4.jpg',
+                        name: '移动4G专区'
+                    }
+                ], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
             ]
         }
     }
@@ -162,6 +196,41 @@ export default {
                     }
                     &:hover{
                         background-color: $colorA;
+                        .children{
+                            display: block;
+                        }
+                    }
+                    .children{
+                        display: none;
+                        width: 962px;
+                        height: 451px;
+                        background-color: $colorG;
+                        position: absolute;
+                        top: 0;
+                        left: 264px;
+                        border: 1px solid $colorH;
+                        ul{
+                            display: flex;
+                            justify-content: space-between;
+                            align-items: center;
+                            height: 75px;
+                            li{
+                                height: 75px;
+                                line-height: 75px;
+                                width: 241px;
+                                padding-left: 23px;
+                            }
+                            a{
+                                color: $colorB;
+                                font-size: J;
+                            }
+                            img{
+                                width: 42px;
+                                height: 35px;
+                                vertical-align: middle;
+                                margin-right: 10px;
+                            }
+                        }
                     }
                 }
             }
