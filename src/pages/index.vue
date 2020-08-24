aff6d0b1b4e10e35d6f4f0e1f9f33179537377a1
EvenyYlzz <172463074.com>
2020-08-25 01:05:49 +0800

    广告banner部分

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -68,8 +68,14 @@
             </swiper>
         </div>
         <div class="ads-box">
+            <a :href="'/#/product/'+item.id" v-for="(item, index) in adsList" :key="index">
+                <img :src="item.img" alt="#">
+            </a>
         </div>
         <div class="banner">
+            <a href="/#/product/30">
+                <img src="/imgs/banner-1.png" alt="#">
+            </a>
         </div>
         <div class="product-box">
         </div>
@@ -156,6 +162,24 @@ export default {
                         name: '移动4G专区'
                     }
                 ], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
+            ],
+            adsList: [
+                {
+                    id: 33,
+                    img: '/imgs/ads/ads-1.png'
+                },
+                {
+                    id: 48,
+                    img: '/imgs/ads/ads-2.jpg'
+                },
+                {
+                    id: 45,
+                    img: '/imgs/ads/ads-3.png'
+                },
+                {
+                    id: 47,
+                    img: '/imgs/ads/ads-4.jpg'
+                }
             ]
         }
     }
@@ -245,6 +269,18 @@ export default {
                 height: 100%;
             }
         }
+    }
+    .ads-box{
+        @include flex();
+        margin-top: 14px;
+        margin-bottom: 31px;
+        a{
+            width: 296px;
+            height: 167px;
+        }
+    }
+    .banner{
+        margin-bottom: 50px;
     }
 }
 </style>
