86e80427787db284a69ef08469eac788bb00928a
EvenyYlzz <172463074.com>
2020-08-24 17:10:40 +0800

    完成swiper1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,99 @@
 <template>
-<div>
+<div class="index">
+    <div class="container">
+        <div class="swiper-box">
+            <swiper :options="swiperOption">
+                <swiper-slide v-for="(item, index) in slideList" :key="index">
+                    <a :href="'/#/product/'+item.id">
+                        <img :src="item.img" alt="#">
+                    </a>
+                </swiper-slide>
+                <div class="swiper-pagination" slot="pagination"></div>
+                <div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>
+            </swiper>
+        </div>
+        <div class="ads-box">
+        </div>
+        <div class="banner">
+        </div>
+        <div class="product-box">
+        </div>
+    </div>
     <service-bar></service-bar>
 </div>
 </template>
 
 <script>
 import ServiceBar from '../components/ServiceBar'
+// import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+// import 'swiper/swiper-bundle.css'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 export default {
-  name: 'index',
-  components: {
-    ServiceBar
-  }
+    name: 'index',
+    components: {
+        ServiceBar,
+        Swiper,
+        SwiperSlide
+    },
+    data() {
+        return {
+            swiperOption: {
+                autoplay: true,
+                loop: true,
+                effect: 'cube',
+                cubeEffect: {
+                    shadowOffset: 100,
+                    shadowScale: 0.6
+                },
+                autoplayDisableOnInteraction: false,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }
+            },
+            slideList: [
+                {
+                    id: '42',
+                    img: '/imgs/slider/slide-1.jpg'
+                },
+                {
+                    id: '45',
+                    img: '/imgs/slider/slide-2.jpg'
+                },
+                {
+                    id: '46',
+                    img: '/imgs/slider/slide-3.jpg'
+                },
+                {
+                    id: '',
+                    img: '/imgs/slider/slide-4.jpg'
+                },
+                {
+                    id: '',
+                    img: '/imgs/slider/slide-5.jpg'
+                }
+            ]
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.index{
+    .swiper-box{
+        .swiper-container{
+            height: 451px;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+}
 </style>
