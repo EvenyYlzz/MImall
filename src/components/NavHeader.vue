c07bd3bf5968bf13ed1946d524e85fe18cee2b67
EvenyYL <172463074.com>
2020-09-09 00:54:18 +0800

    完成页面退出功能及优化

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,6 +11,7 @@
                 <div class="topbar-user">
                     <a href="javascript:;" v-if="username">{{username}}</a>
                     <a href="javascript:;" v-if="!username" @click="login">登录</a>
+                    <a href="javascript:;" v-if="username" @click="logout">退出</a>
                     <a href="javascript:;">我的订单</a>
                     <a href="javascript:;" class="my-cart" @click="goToCart"><span class="icon-cart"></span>购物车({{cartCount}})</a>
                 </div>
@@ -198,6 +199,11 @@ export default {
     },
     mounted() {
         this.getProductList()
+        const params = this.$route.params
+        if (params && params.from === 'login') {
+            this.getCartCount()
+            //  避免重复获取浪费性能资源
+        }
     },
     methods: {
         getProductList() {
@@ -210,6 +216,20 @@ export default {
                 if (res.list.length > 6) { //  拦截截取6条数据，因为顶部手机展示不需要那么多条数据
                     this.phoneList = res.list.slice(0, 6)
                 }
+            })
+        },
+        getCartCount() {
+            this.axios.get('/carts/products/sum').then((res = 0) => {
+                this.$store.dispatch('saveCartCount', res)
+                //  todo 获取购物车商品数量保存到vuex里面
+            })
+        },
+        logout() {
+            this.axios.post('/user/logout').then(() => {
+                this.$message.success('退出成功')
+                this.$cookie.set('userId', '', { expires: '-1' }) // 设置为-1即刻过期
+                this.$store.dispatch('saveUserName', '')
+                this.$store.dispatch('saveCartCount', '0')
             })
         },
         goToCart() {
