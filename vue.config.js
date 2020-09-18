06aff0bb6a588a0470093db7426562564a34b2a9
EvenyYL <172463074@qq.com>
2020-09-19 01:24:23 +0800

    vue.config.js介绍

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,14 @@ module.exports = {
                 }
             }
         }
-    }
+    },
+    //  路由根路径前添加
+    // publicPath: '/app',
+    //  输出文件夹
+    // outputDir: 'dist',
+    //  输出单一的文件页名
+    // indexPath: 'index.html',
+    //  lintOnSave，eslint开关
+    //  开关生成map文件，以防别人通过F12查看源码分析业务、代码
+    productionSourceMap: false
 }
